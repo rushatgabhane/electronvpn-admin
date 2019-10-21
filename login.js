94de1967f9f03735b6c9f533e6af8e849b850777
rushatgabhane <rushatgabhane@gmail.com>
2019-10-21 18:54:10 +0530

    rm login

--- login.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// @route  GET admin/
-// @desc   
-// @access Public
-router.get('/', (req, res) => {
-    res.render('index');
-});
