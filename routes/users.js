5a0c7921df67a9c502e8ea752207d111996cf912
rushatgabhane <rushatgabhane@gmail.com>
2019-10-31 06:33:58 +0530

    display user count

--- routes/users.js
+++ routes/users.js
@@ -10,44 +10,82 @@ router.all('*', helper.isLoggedIn);
 // @desc   Get info of all users, with filter(search) functionality
 // @access Private (Admin only)
 router.get('/', (req, res) => {
-    User.find({}, (err, users) => {
+    // User.find({}, (err, users) => {
+    //     if(err) {
+    //         console.error('err');
+    //         //do something...
+    //     }
+    //     users.map(user => {
+    //         res.send(user.username);
+    //     });
+    // });
+    User.count( (err, count) => {
         if(err) {
-            console.error('err');
-            //do something...
+            console.log('err');
         }
-        users.map(user => {
-            console.log(user.username);
-        });
+        res.send(`We have ${count} users`);
     });
-    res.send('get all users');
 });
 
 // @route  POST /users/adduser
 // @desc   Add a new user
 // @access Private (Admin only)
 router.post('/adduser', (req, res) => {
-    res.send('admin adduser');
-    res.sendStatus(201);
+    let electronServerUserAdd = `http://${electronServerIp}/useradd/${phoneNumber}/none/vpn1/${password}`;
+    request.get(electronServerUserAdd, function (error, response, body) {
+        if (response == null) {
+            res.status(500).send("Unable to communicate with ElectronServer whilst adding user");
+        }
+        else if(response.statusCode == 200){
+            console.log("Added to SE database successfully!");
+            // Add to MongoDB
+            User.register(new User({
+                name: req.body.username,
+                phone: phoneNumber,
+                email: email,
+                regno: regNo,
+                exptime: utcTimeAfterThirtyDays.toString(10),
+                data: "50000000000", //in bytes
+                // data: "100000000", // for development 100 mb
+                vpnhub: "vpn1",
+                description: "none",
+                groupname: "none",
+                ispresent: "1",
+                username: phoneNumber, // Phone is the username
+                plainpassword: password, // Plaintext password for re-adding user to ElectronServer
+                collegename: "mnnit",
+                dataused: "0",
+                amountpaid: "0"
+            }), 
+            password, function (err, websiteuser) {
+                if(err){
+                    console.log(err);
+                    return res.send('passport returned erro');
+                }
+                console.log('added user but didn\'t send a mail');
+                res.send('user added!');
+            });
+        }
+    });
 });
-
-// @route  PUT /users/updateuser
+// @route  POST /users/updateuser
 // @desc   Update an existing user
 // @access Private (Admin only)
-router.put('/updateuser', (req, res) => {
+router.post('/updateuser', (req, res) => {
     res.send('update');
 });
 
-// @route  DELETE /users/deleteuser
+// @route  POST /users/deleteuser
 // @desc   Delete an existing user
 // @access Private (Admin only)
-router.delete('/deleteuser', (req, res) => {
+router.post('/deleteuser', (req, res) => {
     res.send('delete');
 });
 
-// @route  PUT /users/updateusers
+// @route  POST /users/updateusers
 // @desc   Update Multiple users satisfying a condition like ispresent
 // @access Private (Admin only)
-router.put('/updateuser', (req, res) => {
+router.post('/updateusers', (req, res) => {
     res.send('update many');
 });
 
